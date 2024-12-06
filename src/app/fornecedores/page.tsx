5fe8a6376fc8b467c7c2d4db74875e6debda67bf
VictoROliveira-Dev <victorramos123@outlook.com>
2024-12-06 11:15:27 -0300

    chore: optmize providers page

--- src/app/fornecedores/page.tsx
+++ src/app/fornecedores/page.tsx
@@ -23,6 +23,55 @@ import {
 } from "@/components/ui/table";
 import Image from "next/image";
 
+type FormField = {
+  label: string;
+  name: string;
+  type?: string;
+  placeholder: string;
+};
+
+const formFields: FormField[] = [
+  {
+    label: "Nome Completo:",
+    name: "nomecompleto",
+    placeholder: "Digite o nome completo...",
+  },
+  {
+    label: "Data Nascimento:",
+    name: "datanascimento",
+    type: "date",
+    placeholder: "",
+  },
+  { label: "CPF:", name: "cpf", placeholder: "Digite o número do CPF..." },
+  {
+    label: "Cidade:",
+    name: "cidade",
+    placeholder: "Digite a cidade e estado...",
+  },
+  { label: "UF:", name: "uf", placeholder: "Digite o Estado..." },
+  { label: "Rua:", name: "rua", placeholder: "Digite a rua..." },
+  { label: "Bairro:", name: "bairro", placeholder: "Digite o bairro..." },
+  { label: "Número:", name: "numero", placeholder: "Digite o número..." },
+  { label: "Telefone:", name: "telefone", placeholder: "Digite o telefone..." },
+];
+
+const FormInput: React.FC<FormField> = ({
+  label,
+  name,
+  type = "text",
+  placeholder,
+}) => (
+  <div className="flex flex-col">
+    <label htmlFor={name}>{label}</label>
+    <Input
+      name={name}
+      className="border-2 font-medium w-[250px]"
+      placeholder={placeholder}
+      type={type}
+    />
+  </div>
+);
+
 export default function Fornecedores() {
   return (
     <section className="bg-[#070180] pt-12 h-[424px] max-h-[1000px]">
@@ -63,29 +112,23 @@ export default function Fornecedores() {
                 <DialogContent className="w-[900px] h-[520px] flex flex-col items-center">
                   <DialogHeader className="mb-5">
                     <DialogTitle className="font-black">
-                      Cadastro de Fornecedor
+                      Cadastro de Cliente
                     </DialogTitle>
                   </DialogHeader>
 
                   <div className="flex flex-wrap gap-4 w-full justify-center">
-                    <div className="flex flex-col">
-                      <label htmlFor="nomecompleto">Nome Completo:</label>
-                      <Input
-                        name="nomecompleto"
-                        className="border-2 font-medium text-white w-[250px]"
-                        placeholder="Digite o nome completo..."
+                    {formFields.map((field) => (
+                      <FormInput
+                        key={field.name}
+                        label={field.label}
+                        name={field.name}
+                        type={field.type}
+                        placeholder={field.placeholder}
                       />
-                    </div>
-                    <div className="flex flex-col">
-                      <label htmlFor="datanascimento">Data Nascimento:</label>
-                      <Input
-                        name="datanascimento"
-                        className="border-2 font-medium w-[250px]"
-                        type="date"
-                      />
-                    </div>
+                    ))}
+
                     <div className="flex flex-col gap-2">
-                      <div className="flex flex-col">
+                      <div>
                         <label htmlFor="documento">Documento:</label>
                         <Input
                           name="documento"
@@ -95,85 +138,14 @@ export default function Fornecedores() {
                       </div>
                       <RadioGroup defaultValue="rg" className="flex">
                         <div className="flex items-center space-x-2">
-                          <RadioGroupItem value="comfortable" id="r2" />
+                          <RadioGroupItem value="rg" id="r2" />
                           <label htmlFor="r2">RG</label>
                         </div>
                         <div className="flex items-center space-x-2">
-                          <RadioGroupItem value="compact" id="r3" />
+                          <RadioGroupItem value="cnh" id="r3" />
                           <label htmlFor="r3">CNH</label>
                         </div>
                       </RadioGroup>
-                    </div>
-
-                    <div className="flex flex-col">
-                      <label htmlFor="cpf">CPF:</label>
-                      <Input
-                        name="cpf"
-                        className="border-2 font-medium text-white w-[250px]"
-                        placeholder="Digite o número do CPF..."
-                      />
-                    </div>
-
-                    <div className="flex flex-col">
-                      <label htmlFor="cidade">Cidade:</label>
-                      <Input
-                        name="cidade"
-                        className="border-2 font-medium text-white w-[250px]"
-                        placeholder="Digite a cidade e estado..."
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <label htmlFor="uf">UF:</label>
-                      <Input
-                        name="uf"
-                        className="border-2 font-medium text-white w-[250px]"
-                        placeholder="Digite o Estado..."
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <label htmlFor="rua">Rua:</label>
-                      <Input
-                        name="rua"
-                        className="border-2 font-medium text-white w-[250px]"
-                        placeholder="Digite a rua..."
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <label htmlFor="rua">Bairro:</label>
-                      <Input
-                        name="rua"
-                        className="border-2 font-medium text-white w-[250px]"
-                        placeholder="Digite o bairro..."
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <label htmlFor="numero">Número:</label>
-                      <Input
-                        name="numero"
-                        className="border-2 font-medium text-white w-[250px]"
-                        placeholder="Digite o número..."
-                      />
-                    </div>
-                    <div className="flex flex-col w-[250px] items-center">
-                      <label htmlFor="banco">Tipo de Pessoa:</label>
-                      <RadioGroup defaultValue="fisica" className="flex">
-                        <div className="flex items-center space-x-2">
-                          <RadioGroupItem value="fisica" id="fisica" />
-                          <label htmlFor="fisica">Física</label>
-                        </div>
-                        <div className="flex items-center space-x-2">
-                          <RadioGroupItem value="juridica" id="r2" />
-                          <label htmlFor="juridica">Jurídica</label>
-                        </div>
-                      </RadioGroup>
-                    </div>
-                    <div className="flex flex-col">
-                      <label htmlFor="telefone">Telefone:</label>
-                      <Input
-                        name="telefone"
-                        className="border-2 font-medium text-white w-[250px]"
-                        placeholder="Digite o telefone.."
-                      />
                     </div>
                   </div>
                   <DialogFooter className="flex items-center gap-2 mt-10">
@@ -241,33 +213,23 @@ export default function Fornecedores() {
                         <DialogContent className="w-[900px] h-[520px] flex flex-col items-center">
                           <DialogHeader className="mb-5">
                             <DialogTitle className="font-black">
-                              Edição de Fornecedor
+                              Edição de Cliente
                             </DialogTitle>
                           </DialogHeader>
 
                           <div className="flex flex-wrap gap-4 w-full justify-center">
-                            <div className="flex flex-col">
-                              <label htmlFor="nomecompleto">
-                                Nome Completo:
-                              </label>
-                              <Input
-                                name="nomecompleto"
-                                className="border-2 font-medium text-white w-[250px]"
-                                placeholder="Digite o nome completo..."
+                            {formFields.map((field) => (
+                              <FormInput
+                                key={field.name}
+                                label={field.label}
+                                name={field.name}
+                                type={field.type}
+                                placeholder={field.placeholder}
                               />
-                            </div>
-                            <div className="flex flex-col">
-                              <label htmlFor="datanascimento">
-                                Data Nascimento:
-                              </label>
-                              <Input
-                                name="datanascimento"
-                                className="border-2 font-medium w-[250px]"
-                                type="date"
-                              />
-                            </div>
+                            ))}
+
                             <div className="flex flex-col gap-2">
-                              <div className="flex flex-col">
+                              <div>
                                 <label htmlFor="documento">Documento:</label>
                                 <Input
                                   name="documento"
@@ -277,90 +239,17 @@ export default function Fornecedores() {
                               </div>
                               <RadioGroup defaultValue="rg" className="flex">
                                 <div className="flex items-center space-x-2">
-                                  <RadioGroupItem value="comfortable" id="r2" />
+                                  <RadioGroupItem value="rg" id="r2" />
                                   <label htmlFor="r2">RG</label>
                                 </div>
                                 <div className="flex items-center space-x-2">
-                                  <RadioGroupItem value="compact" id="r3" />
+                                  <RadioGroupItem value="cnh" id="r3" />
                                   <label htmlFor="r3">CNH</label>
                                 </div>
                               </RadioGroup>
                             </div>
-
-                            <div className="flex flex-col">
-                              <label htmlFor="cpf">CPF:</label>
-                              <Input
-                                name="cpf"
-                                className="border-2 font-medium text-white w-[250px]"
-                                placeholder="Digite o número do CPF..."
-                              />
-                            </div>
-
-                            <div className="flex flex-col">
-                              <label htmlFor="cidade">Cidade:</label>
-                              <Input
-                                name="cidade"
-                                className="border-2 font-medium text-white w-[250px]"
-                                placeholder="Digite a cidade e estado..."
-                              />
-                            </div>
-                            <div className="flex flex-col">
-                              <label htmlFor="uf">UF:</label>
-                              <Input
-                                name="uf"
-                                className="border-2 font-medium text-white w-[250px]"
-                                placeholder="Digite o Estado..."
-                              />
-                            </div>
-                            <div className="flex flex-col">
-                              <label htmlFor="rua">Rua:</label>
-                              <Input
-                                name="rua"
-                                className="border-2 font-medium text-white w-[250px]"
-                                placeholder="Digite a rua..."
-                              />
-                            </div>
-                            <div className="flex flex-col">
-                              <label htmlFor="rua">Bairro:</label>
-                              <Input
-                                name="rua"
-                                className="border-2 font-medium text-white w-[250px]"
-                                placeholder="Digite o bairro..."
-                              />
-                            </div>
-                            <div className="flex flex-col">
-                              <label htmlFor="numero">Número:</label>
-                              <Input
-                                name="numero"
-                                className="border-2 font-medium text-white w-[250px]"
-                                placeholder="Digite o número..."
-                              />
-                            </div>
-                            <div className="flex flex-col w-[250px] items-center">
-                              <label htmlFor="banco">Tipo de Pessoa:</label>
-                              <RadioGroup
-                                defaultValue="fisica"
-                                className="flex"
-                              >
-                                <div className="flex items-center space-x-2">
-                                  <RadioGroupItem value="fisica" id="fisica" />
-                                  <label htmlFor="fisica">Física</label>
-                                </div>
-                                <div className="flex items-center space-x-2">
-                                  <RadioGroupItem value="juridica" id="r2" />
-                                  <label htmlFor="juridica">Jurídica</label>
-                                </div>
-                              </RadioGroup>
-                            </div>
-                            <div className="flex flex-col">
-                              <label htmlFor="telefone">Telefone:</label>
-                              <Input
-                                name="telefone"
-                                className="border-2 font-medium text-white w-[250px]"
-                                placeholder="Digite o telefone.."
-                              />
-                            </div>
                           </div>
+
                           <DialogFooter className="flex items-center gap-2 mt-10">
                             <Button variant="outline">Fechar</Button>
                             <Button>Salvar</Button>
