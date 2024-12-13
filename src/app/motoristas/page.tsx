5c603c933a0c0a536439a99d73a28e3d96f2b300
VictoROliveira-Dev <victorramos123@outlook.com>
2024-12-13 14:56:23 -0300

    feat: add loading effect and toast motoristas page

--- src/app/motoristas/page.tsx
+++ src/app/motoristas/page.tsx
@@ -15,19 +15,25 @@ import { useEffect, useState } from "react";
 import { api } from "@/lib/axios";
 import { Motorista } from "@/lib/types";
 import DialogRemover from "./components/dialog-remover";
+import loading from "../assets/loading-dark.svg";
+import Image from "next/image";
 
 export default function Motoristas() {
   const [motoristas, setMotoristas] = useState<Motorista[]>([]);
   const [buscarMotorista, setBuscarMotorista] = useState("");
+  const [carregando, setCarregando] = useState(false);
 
   useEffect(() => {
     const fetchMotoristas = async () => {
+      setCarregando(true);
       try {
         const response = await api.get("/motorista");
         setMotoristas(response.data.data);
         console.log("Motoristas:", response.data.data);
       } catch (error) {
         console.error("Erro ao buscar motoristas:", error);
+      } finally {
+        setCarregando(false);
       }
     };
     fetchMotoristas();
@@ -39,7 +45,7 @@ export default function Motoristas() {
     }
 
     return motorista.nome.toLowerCase().includes(buscarMotorista.toLowerCase());
-  })
+  });
 
   return (
     <section className="bg-[#070180] pt-12 h-[425px]">
@@ -66,58 +72,71 @@ export default function Motoristas() {
                 motoristas={motoristas}
               />
             </div>
-            <div className="h-[200px] overflow-y-scroll">
-              <Table>
-                <TableHeader className="border-b-2">
-                  <TableRow>
-                    <TableHead className="text-black font-bold text-center">
-                      Nome
-                    </TableHead>
-                    <TableHead className="text-black font-bold text-center">
-                      CPF
-                    </TableHead>
-                    <TableHead className="text-black font-bold text-center">
-                      Cidade
-                    </TableHead>
-                    <TableHead className="text-black font-bold text-center">
-                      UF
-                    </TableHead>
-                    <TableHead className="text-black font-bold text-center">
-                      Telefone
-                    </TableHead>
-                    <TableHead className="text-black font-bold text-center">
-                      CNH
-                    </TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody className="text-center">
-                  {filtroMotoristas.map((motorista, index) => (
-                    <TableRow key={motorista.id} className="hover:bg-gray-200">
-                      <TableCell>{motorista.nome}</TableCell>
-                      <TableCell>{motorista.cpf}</TableCell>
-                      <TableCell>{motorista.endereco.cidade}</TableCell>
-                      <TableCell>{motorista.endereco.uf}</TableCell>
-                      <TableCell>{motorista.telefone}</TableCell>
-                      <TableCell>{motorista.documento.documento}</TableCell>
-                      <TableCell>
-                        <div className="flex items-center gap-2">
-                          <DialogEditar
-                            motorista={motorista}
-                            setMotoristas={setMotoristas}
-                            motoristas={motoristas}
-                          />
-                          <DialogRemover
-                            motorista={motorista}
-                            setMotoristas={setMotoristas}
-                          />
-                          <DialogInformacoes motoristaId={motorista.id} />
-                        </div>
-                      </TableCell>
+            {carregando ? (
+              <div className="flex items-center justify-center p-10">
+                <Image
+                  src={loading}
+                  alt="loading"
+                  className="text-center animate-spin"
+                />
+              </div>
+            ) : (
+              <div className="h-[200px] overflow-y-scroll">
+                <Table>
+                  <TableHeader className="border-b-2">
+                    <TableRow>
+                      <TableHead className="text-black font-bold text-center">
+                        Nome
+                      </TableHead>
+                      <TableHead className="text-black font-bold text-center">
+                        CPF
+                      </TableHead>
+                      <TableHead className="text-black font-bold text-center">
+                        Cidade
+                      </TableHead>
+                      <TableHead className="text-black font-bold text-center">
+                        UF
+                      </TableHead>
+                      <TableHead className="text-black font-bold text-center">
+                        Telefone
+                      </TableHead>
+                      <TableHead className="text-black font-bold text-center">
+                        CNH
+                      </TableHead>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
+                  </TableHeader>
+                  <TableBody className="text-center">
+                    {filtroMotoristas.map((motorista, index) => (
+                      <TableRow
+                        key={motorista.id}
+                        className="hover:bg-gray-200"
+                      >
+                        <TableCell>{motorista.nome}</TableCell>
+                        <TableCell>{motorista.cpf}</TableCell>
+                        <TableCell>{motorista.endereco.cidade}</TableCell>
+                        <TableCell>{motorista.endereco.uf}</TableCell>
+                        <TableCell>{motorista.telefone}</TableCell>
+                        <TableCell>{motorista.documento.documento}</TableCell>
+                        <TableCell>
+                          <div className="flex items-center gap-2">
+                            <DialogEditar
+                              motorista={motorista}
+                              setMotoristas={setMotoristas}
+                              motoristas={motoristas}
+                            />
+                            <DialogRemover
+                              motorista={motorista}
+                              setMotoristas={setMotoristas}
+                            />
+                            <DialogInformacoes motoristaId={motorista.id} />
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </div>
+            )}
           </div>
         </div>
       </div>
