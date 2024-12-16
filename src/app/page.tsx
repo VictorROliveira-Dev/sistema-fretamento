1cccabcab669e7e13c7b204ff9478546cfa0a8de
VictoROliveira-Dev <victorramos123@outlook.com>
2024-12-16 16:01:10 -0300

    feat: consuming api of travels in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import CustomTable from "@/components/custom-table";
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -9,7 +10,8 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { api } from "@/lib/axios";
-import { IDocumentos } from "@/lib/types";
+import { IDocumentos, Viagem } from "@/lib/types";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const invoices = [
@@ -47,38 +49,79 @@ const invoices = [
 
 export default function Home() {
   const [documentos, setDocumentos] = useState<IDocumentos[]>([]);
+  const [viagens, setViagens] = useState<Viagem[]>([]);
+  const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+
+
 
   useEffect(() => {
-    const fetchDocumentos = async () => {
+    const fetchData = async () => {
       try {
-        const response = await api.get("/documento");
-        setDocumentos(response.data.data);
+        const [documentosReponse, viagensResponse] = await Promise.all([
+          api.get("/documento"),
+          api.get("/viagem"),
+        ]);
+        setDocumentos(documentosReponse.data.data);
+        setViagens(viagensResponse.data.data);
       } catch (error) {
-        console.log("erro ao tentar recuperar documentos", error);
+        console.log("erro ao tentar recuperar dados", error);
       }
     };
 
-    fetchDocumentos();
+    fetchData();
   }, []);
 
   return (
     <>
       <main className="h-[424px] max-h-[500px] bg-[#070180] pt-10">
         <div className="h-[350px] w-[1200px] mx-auto rounded-md bg-white flex items-center justify-around">
-          <CustomTable
-            title="Viagens/Serviços nos próximos 30 dias"
-            headers={["Dia", "Data", "Orçamento", "Veículo", "Motorista"]}
-            rows={invoices}
-            renderRow={(invoice) => (
-              <>
-                <TableCell>{invoice.invoice}</TableCell>
-                <TableCell>{invoice.paymentStatus}</TableCell>
-                <TableCell>{invoice.paymentMethod}</TableCell>
-                <TableCell>{invoice.totalAmount}</TableCell>
-                <TableCell>{invoice.totalAmount}</TableCell>
-              </>
-            )}
-          />
+          <div className="w-[380px] h-[300px] rounded-md shadow-lg shadow-black/40 flex flex-col items-center gap-4">
+            <p className="font-bold">Viagens/Serviços</p>
+            <div className="h-[200px] overflow-y-scroll scrollbar-hide">
+              <Table>
+                <TableHeader>
+                  <TableRow className="bg-white">
+                    <TableHead className="text-black font-black text-center">
+                      Dia
+                    </TableHead>
+                    <TableHead className="text-black font-black text-center">
+                      Data
+                    </TableHead>
+                    <TableHead className="text-black font-black text-center">
+                      Orçamento
+                    </TableHead>
+                    <TableHead className="text-black font-black text-center">
+                      Tipo Viagem
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody className="bg-white text-center">
+                  {viagens.map((viagem) => (
+                    <TableRow key={viagem.id}>
+                      <TableCell>
+                      {diasDaSemana[new Date(viagem.dataHorarioSaida.data).getDay()]} 
+                      </TableCell>
+                      <TableCell>
+                        {new Date(
+                          viagem.dataHorarioSaida.data
+                        ).toLocaleDateString("pt-BR")}
+                      </TableCell>
+                      <TableCell>{viagem.valorContratado}</TableCell>
+                      <TableCell>{viagem.tipoViagem}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+            <div className="flex justify-center">
+              <Link
+                href="/viagens-servicos"
+                className="bg-black text-white text-sm w-20 text-center p-2 rounded-md hover:bg-black/85 transition-all"
+              >
+                Ver mais
+              </Link>
+            </div>
+          </div>
           <CustomTable
             title="Veículos utilizados nos próximos 30 dias"
             headers={["Dia", "Data", "Placa"]}
@@ -126,6 +169,14 @@ export default function Home() {
                   ))}
                 </TableBody>
               </Table>
+            </div>
+            <div className="flex justify-center">
+              <Link
+                href="/documentos"
+                className="bg-black text-white text-sm w-20 text-center p-2 rounded-md hover:bg-black/85 transition-all"
+              >
+                Ver mais
+              </Link>
             </div>
           </div>
         </div>
