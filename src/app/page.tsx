05f595eb3aec7514ab099c4f3f57cbd1036fbe39
VictoROliveira-Dev <victorramos123@outlook.com>
2024-12-16 16:55:56 -0300

    feat: add loading effect and toast in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import CustomTable from "@/components/custom-table";
-import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -11,49 +9,20 @@ import {
 } from "@/components/ui/table";
 import { api } from "@/lib/axios";
 import { IDocumentos, Viagem } from "@/lib/types";
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-];
+import loading from "./assets/loading-dark.svg";
 
 export default function Home() {
   const [documentos, setDocumentos] = useState<IDocumentos[]>([]);
   const [viagens, setViagens] = useState<Viagem[]>([]);
   const diasDaSemana = ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SÁB"];
+  const [carregando, setCarregando] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setCarregando(true);
       try {
         const [documentosReponse, viagensResponse] = await Promise.all([
           api.get("/documento"),
@@ -64,16 +33,20 @@ export default function Home() {
         const mesAtual = dataAtual.getMonth(); // Mês atual (0 = Janeiro, 11 = Dezembro)
         const anoAtual = dataAtual.getFullYear(); // Ano atual
         // Filtrando apenas as viagens do mês atual
-        const viagensDoMesAtual = viagensResponse.data.data.filter((viagem: Viagem) => {
-          const dataViagem = new Date(viagem.dataHorarioSaida.data);
-          const mesViagem = dataViagem.getMonth();
-          const anoViagem = dataViagem.getFullYear();
-          return mesViagem === mesAtual && anoViagem === anoAtual;
-        });
+        const viagensDoMesAtual = viagensResponse.data.data.filter(
+          (viagem: Viagem) => {
+            const dataViagem = new Date(viagem.dataHorarioSaida.data);
+            const mesViagem = dataViagem.getMonth();
+            const anoViagem = dataViagem.getFullYear();
+            return mesViagem === mesAtual && anoViagem === anoAtual;
+          }
+        );
         setDocumentos(documentosReponse.data.data);
         setViagens(viagensDoMesAtual);
       } catch (error) {
         console.log("erro ao tentar recuperar dados", error);
+      } finally {
+        setCarregando(false);
       }
     };
 
@@ -87,50 +60,60 @@ export default function Home() {
           <div className="w-[380px] h-[300px] rounded-md shadow-lg shadow-black/40 flex flex-col items-center gap-4">
             <p className="font-bold">Viagens/Serviços</p>
             <div className="h-[200px] overflow-y-scroll scrollbar-hide">
-              <Table>
-                <TableHeader>
-                  <TableRow className="bg-white">
-                    <TableHead className="text-black font-black text-center">
-                      Dia
-                    </TableHead>
-                    <TableHead className="text-black font-black text-center">
-                      Data
-                    </TableHead>
-                    <TableHead className="text-black font-black text-center">
-                      Orçamento
-                    </TableHead>
-                    <TableHead className="text-black font-black text-center">
-                      Tipo Viagem
-                    </TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody className="bg-white text-center">
-                  {viagens.map((viagem) => (
-                    <TableRow key={viagem.id}>
-                      <TableCell>
-                        {
-                          diasDaSemana[
-                            new Date(viagem.dataHorarioSaida.data).getDay()
-                          ]
-                        }
-                      </TableCell>
-                      <TableCell>
-                        {new Date(
-                          viagem.dataHorarioSaida.data
-                        ).toLocaleDateString("pt-BR")}
-                      </TableCell>
-                      <TableCell>
-                        {" "}
-                        {viagem.valorContratado.toLocaleString("pt-BR", {
-                          style: "currency",
-                          currency: "BRL",
-                        })}
-                      </TableCell>
-                      <TableCell>{viagem.tipoViagem}</TableCell>
+              {carregando ? (
+                <div className="flex items-center justify-center">
+                  <Image
+                    src={loading}
+                    alt="carregando"
+                    className="text-center animate-spin"
+                  />
+                </div>
+              ) : (
+                <Table>
+                  <TableHeader>
+                    <TableRow className="bg-white">
+                      <TableHead className="text-black font-black text-center">
+                        Dia
+                      </TableHead>
+                      <TableHead className="text-black font-black text-center">
+                        Data
+                      </TableHead>
+                      <TableHead className="text-black font-black text-center">
+                        Orçamento
+                      </TableHead>
+                      <TableHead className="text-black font-black text-center">
+                        Tipo Viagem
+                      </TableHead>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
+                  </TableHeader>
+                  <TableBody className="bg-white text-center">
+                    {viagens.map((viagem) => (
+                      <TableRow key={viagem.id}>
+                        <TableCell>
+                          {
+                            diasDaSemana[
+                              new Date(viagem.dataHorarioSaida.data).getDay()
+                            ]
+                          }
+                        </TableCell>
+                        <TableCell>
+                          {new Date(
+                            viagem.dataHorarioSaida.data
+                          ).toLocaleDateString("pt-BR")}
+                        </TableCell>
+                        <TableCell>
+                          {" "}
+                          {viagem.valorContratado.toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                          })}
+                        </TableCell>
+                        <TableCell>{viagem.tipoViagem}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              )}
             </div>
             <div className="flex justify-center">
               <Link
@@ -145,34 +128,44 @@ export default function Home() {
           <div className="w-[380px] h-[300px] rounded-md shadow-lg shadow-black/40 flex flex-col items-center gap-4">
             <p className="font-bold">Vencimento Doc/Certificados</p>
             <div className="h-[200px] overflow-y-scroll scrollbar-hide">
-              <Table>
-                <TableHeader>
-                  <TableRow className="bg-white">
-                    <TableHead className="text-black font-black text-center">
-                      Vencimento
-                    </TableHead>
-                    <TableHead className="text-black font-black text-center">
-                      Referência
-                    </TableHead>
-                    <TableHead className="text-black font-black text-center">
-                      Doc/Certificados
-                    </TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody className="bg-white text-center">
-                  {documentos.map((documento) => (
-                    <TableRow key={documento.id}>
-                      <TableCell>
-                        {new Date(documento.vencimento).toLocaleDateString(
-                          "pt-BR"
-                        )}
-                      </TableCell>
-                      <TableCell>{documento.referencia}</TableCell>
-                      <TableCell>{documento.tipoDocumento}</TableCell>
+              {carregando ? (
+                <div className="flex items-center justify-center">
+                  <Image
+                    src={loading}
+                    alt="carregando"
+                    className="text-center animate-spin"
+                  />
+                </div>
+              ) : (
+                <Table>
+                  <TableHeader>
+                    <TableRow className="bg-white">
+                      <TableHead className="text-black font-black text-center">
+                        Vencimento
+                      </TableHead>
+                      <TableHead className="text-black font-black text-center">
+                        Referência
+                      </TableHead>
+                      <TableHead className="text-black font-black text-center">
+                        Doc/Certificados
+                      </TableHead>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
+                  </TableHeader>
+                  <TableBody className="bg-white text-center">
+                    {documentos.map((documento) => (
+                      <TableRow key={documento.id}>
+                        <TableCell>
+                          {new Date(documento.vencimento).toLocaleDateString(
+                            "pt-BR"
+                          )}
+                        </TableCell>
+                        <TableCell>{documento.referencia}</TableCell>
+                        <TableCell>{documento.tipoDocumento}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              )}
             </div>
             <div className="flex justify-center">
               <Link
