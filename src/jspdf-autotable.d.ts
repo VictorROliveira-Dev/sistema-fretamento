4dd74c04570f85c5a5bbbaf75fee15449141e605
VictoROliveira-Dev <victorramos123@outlook.com>
2024-12-27 17:35:56 -0300

    chore: adjust document-dialog despesa

--- src/jspdf-autotable.d.ts
+++ src/jspdf-autotable.d.ts
@@ -1,5 +1,7 @@
-declare module 'jspdf' {
+import "jspdf"; // Importa o tipo base do jsPDF
+
+declare module "jspdf" {
   interface jsPDF {
-    autoTable: unknown; 
+    autoTable: (columns: string[], data: any[][], options?: any) => void; // Tipagem básica
   }
 }
