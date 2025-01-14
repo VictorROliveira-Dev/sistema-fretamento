65d9fc03bad7f36985577087898326f9f67d4a14
VictoROliveira-Dev <victorramos123@outlook.com>
2025-01-14 12:29:48 -0300

    chore: adjust bug in error in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -54,6 +54,7 @@ export default function Login() {
 
         toast.success("Login bem-sucedido!");
       }
+     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const errorMessage =
